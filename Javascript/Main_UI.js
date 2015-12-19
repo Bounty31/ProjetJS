f9e3f578bd1aaf7b19131386f509a65144341523
Bounty31 <daniel.beltzer@orange.fr>
2015-12-19 15:00:57 +0100

    New UI
    Base layout (menu + animations) and some responsiveness

--- Javascript/Main_UI.js
+++ Javascript/Main_UI.js
@@ -1,3 +1,35 @@
-$(document).ready(function(){
-	$('.owl-carousel').owlCarousel();
+var hover;
+var dropAnimation = 0;
+var hoverAnimation = 0;
+
+
+
+$(document).ready(function() {
+    hover = false;
+    TweenLite.set($(".game_img"), {scale:0});
+
+    /* Dropdown animations */
+    $(".drop").mouseenter(function(e) {
+        if (!hover) {
+            animationDropIn(dropAnimation);
+            hover = true;
+        }
+    });
+    $(".drop").mouseleave(function(e) {
+        if (e.toElement != null &&
+                !e.toElement.classList.contains("drop") &&
+                !e.toElement.classList.contains("game")) {
+            animationDropOut(dropAnimation);
+            hover = false;
+        }
+    });
+
+    /* Image hover animations */
+    $(".game img").hover(function() {
+            animationHoverIn(this, hoverAnimation);
+        },
+        function() {
+            animationHoverOut(this, hoverAnimation);
+        });
+
 });
