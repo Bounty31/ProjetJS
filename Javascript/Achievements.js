24f8c95d29619d926ceb64b5f6393c892fe9ecda
Bounty31 <daniel.beltzer@orange.fr>
2016-01-04 10:28:02 +0100

    V 0.7.1 FIxes achievements

--- Javascript/Achievements.js
+++ Javascript/Achievements.js
@@ -14,33 +14,34 @@ function Achievements() {
     //à revoir pour la gestion des chemins
     $.getJSON("../Achievements/achievements.json").done(function(json) {
         self.achievements_list = json.achievements;
+        console.log(json.achievements);
     });
 };
 
 //Recupere l'achievement correspondant dans le tableau json
-Achievements.prototype.get = function(index) {
+Achievements.prototype.get = function(index, temps) {
     this.currentAchievement = this.achievements_list[index];
     this.text.innerHTML = this.currentAchievement["name"];
     this.icon.className = this.currentAchievement["icon"];
-    this.rise();
+    this.rise(temps);
 };
 
 //Animation du cadre achievement
-Achievements.prototype.rise = function() {
+Achievements.prototype.rise = function(temps) {
     var self = this;
-    TweenMax.fromTo(self.div,1,{y:0},{y:-65,ease:Elastic.easeOut.config(1, 0.8)});
+    TweenMax.fromTo(self.div,0.85,{y:0},{y:-65,ease:Elastic.easeOut.config(1, 0.6)});
     setTimeout(function(){
-        TweenMax.fromTo(self.div,1,{y:-65},{y:0,ease:Elastic.easeIn.config(1, 0.8)})
-    },1618);
+        TweenMax.fromTo(self.div,0.9,{y:-65},{y:0,ease:Elastic.easeIn.config(1, 1)})
+    }, temps);
     //idée : faire un overshoot lors de l'apparition, ensuite même chose pour disparaitre
 };
 
 //****************à supprimer lors de l'implementation des achievements***************
 //Fonction test lancé lorsque l'on appuie sur le bouton Achivement en remplissant 
 //l'input correspondant
-function RiseAchievementViaButton() {
+function RiseAchievementViaButton(temps) {
     var index = document.getElementById("inputTestIndex").value;
-    mAchievements.get(index);
+    mAchievements.get(index, temps);
 }
 // à declarer en tout debut de document
 var mAchievements = new Achievements();
